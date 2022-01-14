d07f8258e9bcf7020adf9611483aa0dd5ea6cf91
pig <lists@luigirosa.com>
2022-01-14 06:05:27 +0100

    via header

--- public/download.php
+++ public/download.php
@@ -6,7 +6,6 @@
 		<link rel='stylesheet' href='static/stili.css' type='text/css'/>
 		<meta http-equiv='X-UA-Compatible' content='IE=edge'/>
 		<meta name='viewport' content='width=device-width, initial-scale=1'/>
-    <link rel="author" href="mailto:sesae@sesae.com">
 		<title>SESAE - Download</title>
 	</head>
 	<body>
